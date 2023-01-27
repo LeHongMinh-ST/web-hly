eaa097603f6b6633d27a61c422c1fcec1ba22581
Lê Hồng Minh <637942@sv.vnua.edu.vn>
2023-01-27 23:23:50 +0700

    layout admin

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class DashboardController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function index(): Factory|View|Application
+    {
+        return view('admin.pages.dashboard');
+    }
+}
