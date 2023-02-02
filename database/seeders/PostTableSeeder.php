0e9a3cc2483ebea3446e3ca93d2b7e085c9a0fa7
uyenvt <uyenvt.vnua@gmail.com>
2023-02-03 00:09:37 +0700

    map category

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostTableSeeder extends Seeder
 {
@@ -15,8 +16,10 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('posts')->truncate();
+        DB::table('post_categories')->truncate();
         for ($i=1; $i<30; $i++){
-            Post::create([
+            $post = Post::create([
                 'title'=>"Lorem Ipsum is simply dummy text of the printing and typesetting industry ".$i,
             'content'=>$i." Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
             'status'=>1,
@@ -27,6 +30,14 @@ class PostTableSeeder extends Seeder
 //            'create_by',
 //            'update_by',
         ]);
+            DB::table('post_categories')->insert([
+                'category_id'=>$i%7 + 1,
+                'post_id'=>$post->id
+            ]);
+            DB::table('post_categories')->insert([
+                'category_id'=>($i+1)%7 + 1,
+                'post_id'=>$post->id
+            ]);
         }
     }
 }
