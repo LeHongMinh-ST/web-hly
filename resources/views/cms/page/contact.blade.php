4e2f6b56e89767f1755bb40ba2f67ff081d2351b
Đàm Đức Chiến <637905@sv.vnua.edu.vn>
2023-02-25 19:11:04 +0700

    create page contact

--- resources/views/cms/page/contact.blade.php
+++ resources/views/cms/page/contact.blade.php
@@ -3,18 +3,106 @@
     Liên hệ - Tập đoàn HLY
 @endsection
 @section('content')
+<div>
 
     <section class="mapContact stagger-up" >
-    <iframe style="width: 100%; height: 600px;"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d329.3071982872466!2d105.78408276636873!3d20.965135468526267!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ad2ae5a35ab9%3A0x98d0fb2dd90f4554!2zNTAtNTIgTmfDtSBIw6AgVHLDrCAyLCBIw6AgQ-G6p3UsIEjDoCDEkMO0bmcsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1676704693945!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        <div class="copy">
-            <h2>T&#7853;p đoàn HLY</h2>
-            <h3>Tr&#7909; s&#7903; chính</h3>
-            <p><strong><i class="fas fas fa-thumbs-up"></i> Đ&#7883;a ch&#7881;:</strong><span>N02 - LK1, Hà Trì, Phường Hà Cầu, Quận Hà Đông, Thành phố Hà Nội, Việt Nam</span></p>
-            <p><strong><i class="fas fa-phone"></i> Đi&#7879;n tho&#7841;i:</strong><span>+84 (24) 0000 0000</span></p>
-            <p><strong><i class="fas fa-fax"></i> Fax:</strong><span>+84 (24) 0000 0000</span></p>
+        <img src="./assets/fe/images/contact.jpg" style="width: 100%;"/>
+        <div style="display: flex; width: 100%; height: 100px; background-color: #0e6447; font-size: 20px; padding: 0 50px; color: #ffffff;">
+        <div style="max-width: 1300px; margin: auto; display: flex; justify-content: space-between" >
+            <div style="width: 30%; font-size: 30px; font-weight: bold;">
+                Tìm hiểu thêm và kết nối
+            </div>
+            <div style="width: 60%; font-size: 18px; font-weight: 400">
+                Để tìm hiểu thêm về  HLY, về các chuyên gia tư vấn, các dịch vụ cung cấp và đánh giá tiềm năng cơ hội phát triển, chúng tôi rất hân hạnh khi được kết nối với quý doanh nghiệp.
+            </div>
         </div>
+    </div>
     </section>
-    <div class="formWrap stagger-up">
+    <div class="" style="width: 100%; margin: auto; display: flex; justify-content: space-between;  ">
+        <div class="address" style="width: 40%; background-color: #f3f3f5; display: flex; justify-content: end; padding-top: 70px; ">
+            <div style="width: 60%;">
+            <p style="font-weight: bold; ">
+                Hà Nội - Trụ sở chính
+            </p><br/>
+            <p>
+                FPT Tower, 10 Phạm Văn Bạch, P. Dịch Vọng, Q. Cầu Giấy, Hà Nội, Việt Nam
+            </p><br/>
+            <p style="font-weight: bold; ">
+                Hà Nội - Trụ sở chính
+            </p><br/>
+            <p>
+                FPT Tower, 10 Phạm Văn Bạch, P. Dịch Vọng, Q. Cầu Giấy, Hà Nội, Việt Nam
+            </p>
+            </div>
+        </div>
+        <!-- <div class="form"> -->
+        <div class="formWrap stagger-up" style="width: 60%; padding: 30px 0;">
+        <h2 class="" style="font-size: 30px; font-weight: bold; text-align: center">Vui lòng điền vào mẫu đơn bên dưới để nhận thêm thông tin về các dịch vụ cung cấp của chúng tôi</h2>
+        <form name="frmContact" class="" style="margin-top: 30px; display: flex; flex-direction: column; gap: 20px;" action="/lien-he" method="POST" >
+           <div style="display: flex;  gap: 40px;">
+            <div style="width: 50%;">
+                    <label for="fullname">Tên *</label>
+                    <input class="form-control" style="border-radius: 2px; border-color: #9e9e9e" type="text" name="fullname"  />
+                </div>
+                <div style="width: 50%;">
+                    <label for="fullname">Họ *</label>
+                    <input class="form-control" style="border-radius: 2px; border-color: #9e9e9e" type="text" name="fullname"  />
+                </div>
+           </div>
+           <div style="display: flex;  gap: 40px;">
+            <div style="width: 50%;">
+                    <label for="fullname">Email doanh nghiệp *</label>
+                    <input class="form-control" style="border-radius: 2px; border-color: #9e9e9e" type="text" name="fullname"  />
+                </div>
+                <div style="width: 50%;">
+                    <label for="fullname">Số điện thoại *</label>
+                    <input class="form-control" style="border-radius: 2px; border-color: #9e9e9e" type="text" name="fullname"  />
+                </div>
+           </div>
+            <div style="width: 100%;">
+                <label for="fullname">Doanh nghiệp *</label>
+                <input class="form-control" style="border-radius: 2px; border-color: #9e9e9e" type="text" name="fullname"  />
+            </div>
+            <div style="width: 100%;">
+                <label for="fullname">Câu hỏi/Thắc mắc cần giải đáp *</label>
+                <input class="form-control" style="border-radius: 2px; border-color: #9e9e9e" type="text" name="fullname"  />
+            </div>
+            <div style="width: 100%;">
+                <label for="fullname">Chủ đề *</label>
+                <input class="form-control" style="border-radius: 2px; border-color: #9e9e9e" type="text" name="fullname"  />
+            </div>
+            <div style="width: 100%;">
+                <label for="fullname">Chi tiết cụ thể về câu hỏi/thắc mắc cần giải đáp *</label>
+                <textarea rows="5" class="form-control" style="border-radius: 2px; border-color: #9e9e9e height: 50px;" type="text" name="fullname"  >
+                </textarea>
+            </div>
+            <div class="g-recaptcha" data-sitekey="6Lftwq0UAAAAAD43amX4uFmDmkTvDM7m7KqQpv1v" data-message="Vui lòng nh&#7845;n ch&#7885;n &quot;Tôi không ph&#7843;i robot&quot;"></div>
+            <div style="width: 100%; text-align: right;">
+                <button type="button" style="margin: 0; max-width: unset; border-color: unset;" class="btn-show-more">Gửi yêu cầu</button>
+            </div>
+        </form>
+        <!-- <form name="frmContact" class="frmContact" action="/lien-he" method="POST">
+            <ul>
+                <li><input type="text" name="Name" class="js-required form-control" placeholder="H&#7885; và tên"></li>
+                <li><input type="text" name="Address" class="js-required form-control" placeholder="Đ&#7883;a ch&#7881;"></li>
+                <li><input type="text" name="Phone" class="js-required js-phone form-control" placeholder="Đi&#7879;n tho&#7841;i"></li>
+                <li><input type="text" name="Email" class="js-required js-email form-control" placeholder="Email"></li>
+                <li><textarea placeholder="N&#7897;i dung" name="Content" rows="5"></textarea></li>
+                <li>
+                    <div class="g-recaptcha" data-sitekey="6Lftwq0UAAAAAD43amX4uFmDmkTvDM7m7KqQpv1v" data-message="Vui lòng nh&#7845;n ch&#7885;n &quot;Tôi không ph&#7843;i robot&quot;"></div>
+                </li>
+            </ul>
+            <div class="btnwrap">
+                <input name="__RequestVerificationToken" type="hidden" value="OaqP-KgeFfn1k_8FbT812SVX7-XplvdpYigFw4YjuSKzSjgrl5PqdUUawjP3s3eHQOqzG3b_jpe27hETyQfFI6FK95-nmL7bE9p7DBEuUbE1" />
+                <a class="btn btn-submit" style="background-color: #008155" href="#"><span>G&#7917;i n&#7897;i dung</span></a>
+            </div>
+        </form> -->
+    </div>
+        <!-- </div> -->
+    </div>
+
+
+    <!-- <div class="formWrap stagger-up">
         <h2 class="title">Liên h&#7879; v&#7899;i chúng tôi</h2>
         <form name="frmContact" class="frmContact" action="/lien-he" method="POST">
             <ul>
@@ -32,6 +120,7 @@
                 <a class="btn btn-submit" style="background-color: #008155" href="#"><span>G&#7917;i n&#7897;i dung</span></a>
             </div>
         </form>
-    </div>
+    </div> -->
+</div>
     <script src="https://www.google.com/recaptcha/api.js?hl=vi" async defer></script>
 @endsection
