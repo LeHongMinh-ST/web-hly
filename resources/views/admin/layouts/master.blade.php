81c8d02aef2afed236d9f7bf2de36e1feed8a472
namnh2 <namnh2@fafu.com.vn>
2023-03-25 16:39:37 +0700

    Sửa lại title của trang quản trị

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -5,8 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-{{--    <title>@yield('title', 'Trang quản trị - HLY')</title>--}}
-    <title>@yield('str')</title>
+    <title>@yield('title', 'Trang quản trị - HLY')</title>
 
     @include('admin.includes.style')
 
